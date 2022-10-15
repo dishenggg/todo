24bf0c66265809499fa2a4e6e26b003163350342
dishenggg <yeodisheng@gmail.com>
2022-10-15 19:22:40 +0800

    Added POST request

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 
 const express = require('express');
+const {v4: uuidv4} = require('uuid')
 const PORT = process.env.PORT || 8080;
 const app = express();
 const api = require('./api');
 
+const todos = [
+    {
+        id: 1,
+        task: "Do A"
+    },
+    {
+        id: 2,
+        task: "Do B"
+    }
+]
+
 app.use('/api', api);
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send(
@@ -13,16 +26,22 @@ app.get('/', (req, res) => {
 });
 
 app.get('/todo', (req, res) => {
-    res.status(200).send({
-        1:'task 1',
-        2:'task 2',
-        3:'task 31'
-    })
+    res.status(200).json(todos)
 });
 
 app.get('/todo/:id', (req, res) => {
     res.status(200).send(req.params.id)
 });
+
+app.post('/', (req, res) => {
+    const newTodo = {
+        id: uuidv4(),
+        task: req.body.task
+    }
+    todos.push(newTodo)
+    res.status(201).json(todos);
+
+})
 
 app.listen(
     PORT,
