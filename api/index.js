39b392a0a2fc3f52245e4dbe339db876bbd53f3b
dishenggg <yeodisheng@gmail.com>
2022-12-21 17:32:30 +0800

    Clean up

--- api/index.js
+++ api/index.js
@@ -1,14 +1,83 @@
+
 const express = require('express');
+const {v4: uuidv4} = require('uuid')
+const PORT = process.env.PORT || 8080;
+const app = express();
+const cors = require('cors');
 
-const router = express.Router();
-const tasks = require('./tasks');
+const todos = [
+    {
+        id: 1,
+        task: "Do A",
+        completed: false
+    },
+    {
+        id: 2,
+        task: "Do B",
+        completed: true
+    }
+];
 
-router.get('/', (req, res) => {
-    res.json({
-        message: 'Hello'
-    })
-})
+app.use(express.json());
+app.use(cors());
 
-router.use('/tasks', tasks);
+app.get('/', (req, res) => {
+    res.status(200).send(
+        "Welcome"
+    )
+});
+// Get all tasks
+app.get('/todo', (req, res) => {
+    res.status(200).json(todos);
+});
+// Get task with id
+app.get('/todo/:id', (req, res) => {
+    let task = todos.find(x => x.id == req.params.id);
+    console.log(task)
+    if (typeof task !== 'undefined') {
+        res.status(200).send(task);
+    } else {
+        res.status(404).send("404 Not Found");
+    }
+});
+// Adds new task to list
+app.post('/todo/new', (req, res) => {
+    const newTodo = {
+        id: uuidv4(),
+        task: req.body.task,
+        completed: false
+    };
+    todos.push(newTodo);
+    res.status(201).json(todos);
 
-module.exports = router;
+});
+// Delete tasks from list
+app.delete('/todo/delete/:id', (req, res) => {
+    let taskToDel = todos.find(x => x.id == req.params.id);
+    if (typeof taskToDel !== 'undefined') {
+        let index = todos.indexOf(taskToDel);
+        todos.splice(index, 1)
+        res.json(todos);
+        res.status(200)
+    } else {
+        res.status(404).send("404 Not Found");
+    }
+});
+
+// Toggle Complete
+app.put('/todo/complete/:id', (req, res) => {
+    let task = todos.find(x => x.id == req.params.id);
+    if (typeof task !== 'undefined') {
+        let index = todos.indexOf(task);
+        todos[index].completed = !todos[index].completed
+        res.json(todos);
+        res.status(200)
+    } else {
+        res.status(404).send("404 Not Found");
+    }
+});
+
+app.listen(
+    PORT,
+    () => console.log(`its alive on http://localhost:${PORT}`)
+);
--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,38 @@
+import { useState, useEffect } from "react";
+
+const API = "http://localhost:8080"
+
 function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState("");
+
+  useEffect(() => {
+    getTodos();
+    console.log(todos)
+  }, []);
+
+  const getTodos = () => {
+    fetch(API + "/todo")
+      .then(res => res.json())
+      .then(data => setTodos(data))
+      .catch(err => console.error(err));
+  };
   return (
     <div className="App">
         <h1>Hello</h1>
         <h3>Todos</h3>
+        <div className="addTask">ADD TASK</div>
 
         <div className="todos">
-
-          <div className="todo"> 
-            <div className="checkbox">check</div>
-            <div className="taskDetails">test</div>
-            <div className="deleteTask">del</div>
-          </div>
-
-          <div className="todo"> 
-            <div className="checkbox">check</div>
-            <div className="taskDetails">apple</div>
-            <div className="deleteTask">del</div>
-          </div>
-
-          <div className="todo"> 
-            <div className="checkbox">check</div>
-            <div className="taskDetails">banana</div>
-            <div className="deleteTask">del</div>
-          </div>
-
+          {todos.map(todo => (
+            <div className={ "todo " + (todo.completed ? "completed" : "")} key = { todo.id }> 
+              <div className="checkbox"></div>
+              <div className="taskDetails">{ todo.task }</div>
+              <div className="modifyTask">ch</div>
+              <div className="deleteTask">x</div>
+            </div>
+          ))}
+          
         </div>
     </div>
   );
