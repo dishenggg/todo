10ac99c82ad34dac2e825cd383dc06838db0fb3f
dishenggg <yeodisheng@gmail.com>
2022-12-24 00:15:11 +0800

    Added edit task, code clean up

--- api/index.js
+++ api/index.js
@@ -1,86 +1,33 @@
 
 const express = require('express');
-const {v4: uuidv4} = require('uuid')
 const PORT = process.env.PORT || 8080;
 const app = express();
-//const cors = require('cors');
+const cors = require('cors');
+const todo = require('./todo.js')
 
-const todos = [
-    {
-        id: 1,
-        task: "Do A",
-        completed: false
-    },
-    {
-        id: 2,
-        task: "Do B",
-        completed: true
-    },
-    {
-        id: 3,
-        task: "Do C",
-        completed: false
+// Cors
+const whitelist = ['http://localhost:3000', "https://todo-five-phi.vercel.app"]
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
     }
-];
+  }
+}
+app.use(cors(corsOptions));
 
+// Middleware
 app.use(express.json());
-//app.use(cors());
+app.use(cors());
 
+// Todo API
+app.use('/todo', todo)
 app.get('/', (req, res) => {
     res.status(200).send(
         "Welcome"
     )
-});
-// Get all tasks
-app.get('/todo', (req, res) => {
-    res.status(200).json(todos);
-});
-// Get task with id
-app.get('/todo/:id', (req, res) => {
-    let task = todos.find(x => x.id == req.params.id);
-    console.log(task)
-    if (typeof task !== 'undefined') {
-        res.status(200).send(task);
-    } else {
-        res.status(404).send("404 Not Found");
-    }
-});
-// Adds new task to list
-app.post('/todo/new', (req, res) => {
-    const newTodo = {
-        id: uuidv4(),
-        task: req.body.task,
-        completed: false
-    };
-    todos.push(newTodo);
-    res.json(todos);
-    res.status(201);
-
-});
-// Delete tasks from list
-app.delete('/todo/delete/:id', (req, res) => {
-    let taskToDel = todos.find(x => x.id == req.params.id);
-    if (typeof taskToDel !== 'undefined') {
-        let index = todos.indexOf(taskToDel);
-        todos.splice(index, 1)
-        res.json(todos);
-        res.status(200)
-    } else {
-        res.status(404).send("404 Not Found");
-    }
-});
-
-// Toggle Complete
-app.put('/todo/complete/:id', (req, res) => {
-    let task = todos.find(x => x.id == req.params.id);
-    if (typeof task !== 'undefined') {
-        let index = todos.indexOf(task);
-        todos[index].completed = !todos[index].completed
-        res.json(todos);
-        res.status(200)
-    } else {
-        res.status(404).send("404 Not Found");
-    }
 });
 
 app.listen(
--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import icon from './img/recycle-bin-line-icon.svg';
+import Footer from './footer';
 
-const API = "https://todo-five-phi.vercel.app"
+//const API = "https://todo-five-phi.vercel.app"
+const API = "http://localhost:8080"
 
 function App() {
   const [todos, setTodos] = useState([]);
   const [newTodo, setNewTodo] = useState("");
+  const [editId, setEditId] = useState(-1);
+  const [editedTodo, setEditedTodo] = useState("");
+  const [ref, setRef] = useState(undefined);
 
   useEffect(() => {
     getTodos();
@@ -19,9 +24,9 @@ function App() {
   };
 
   const toggleComplete = async id => {
-    const toggledTodo = await fetch(API + "/todo/complete/" + id, {method : "PUT"})
+    const updatedTodos = await fetch(API + "/todo/complete/" + id, {method : "PATCH"})
       .then(res => res.json());
-    setTodos(toggledTodo);
+    setTodos(updatedTodos);
 
   };
 
@@ -32,6 +37,7 @@ function App() {
   };
 
   const addTodo = async () => {
+    if (newTodo === "") {return}
     const updatedTodos = await fetch(API + "/todo/new", {
       method: "POST",
       headers: {
@@ -45,34 +51,62 @@ function App() {
     setTodos(updatedTodos);
   };
 
-  const handleEnterKey = (event) => {
-    if (event.key === "Enter" && newTodo !== "") {
-      addTodo();
+  const updateTodo = async () => {
+    const updatedTodos = await fetch(API + "/todo/edit/" + editId, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        task: editedTodo
+      })
+    }).then(res => res.json())
+    setTodos(updatedTodos)
+    setEditId(-1)
+    setEditedTodo("")
+  };
+
+  const handleEnterKey = (fn ,event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      fn();
     }
-  }
+  };
+  
+  const handleClickOnTask = (id, task, event) => {
+    setEditId(id);
+    setEditedTodo(task);
+  };
 
   return (
     <div className="App">
-        <h1>Hello</h1>
+        <h1>Welcome</h1>
         <h3>Todos</h3>
         
         <div className="addTask">
-          <input className="addNewTodo" input="text" placeholder="+ Add New Task" onKeyDown={handleEnterKey} onChange={val => setNewTodo(val.target.value)} value={newTodo}></input>
+          <input className="addNewTodo" input="text" placeholder="+ Add New Task" onKeyDown={e => handleEnterKey(addTodo, e)} onChange={e => setNewTodo(e.target.value)} value={newTodo}></input>
         </div>
 
         <div className="todos">
           {todos.length > 0 ? todos.map(todo => (
-            <div className={ "todo " + (todo.completed ? "completed" : "")} key = { todo.id }> 
+            <div className={"todo " + (todo.completed ? "completed" : "")} key = {todo.id}> 
               <div className="checkbox-area" onClick = {() => toggleComplete(todo.id)}> 
                 <div className="checkbox" ></div>
               </div>
-              <div className="taskDetails">{ todo.task }</div>
-              <div className="modifyTask">edit</div>
+
+              {
+                editId === todo.id ? (
+                  <div contentEditable="true" autoFocus suppressContentEditableWarning={true}  className="editTodo" input="text" placeholder={todo.task} onKeyDown={e => handleEnterKey(updateTodo, e)} onInput={e => setEditedTodo(e.currentTarget.textContent)} onBlur={() => updateTodo}value={editedTodo}>{todo.task}</div>
+                 ) : (
+                  <div className="taskDetails" onClick={e => handleClickOnTask(todo.id, todo.task, e)}>{ todo.task }</div>
+                 )
+              }
               <div className="deleteTask" onClick={() => deleteTodo(todo.id)}><img src={icon} alt="trash icon"></img></div>
             </div>
           )) : "You Have No Tasks!"}
           
         </div>
+        <Footer />
     </div>
   );
 }
